4284c018fbf37a1a56677ee641a539340f331e14
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2023-03-30 22:54:37 -0600

    Created an array list
    I created the start of my Pokemon list in my script.js file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-alert("Hello world");
-
-let favoriteFood = "Chipotle";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Charmander", height: 0.6, type: "Fire" },
+  { name: "Squirtle", height: 0.5, type: "Water" },
+  { name: "Nidoqueen", height: 1.3, type: ["Ground", "Poison"] },
+  { name: "Venomoth", height: 1.5, type: ["Bug", "Poison"] },
+  { name: "Girafarig", height: 1.5, type: ["Psychic", "Normal"] },
+];
