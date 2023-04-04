e7c715c8d39083ed019767a97100474892030006
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2023-04-04 11:14:34 -0600

    Created for loop

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//This is the start of my script
+
 let pokemonList = [
   { name: "Charmander", height: 0.6, type: ["Fire"] },
   { name: "Squirtle", height: 0.5, type: ["Water"] },
@@ -5,3 +7,9 @@ let pokemonList = [
   { name: "Venomoth", height: 1.5, type: ["Bug", "Poison"] },
   { name: "Girafarig", height: 1.5, type: ["Psychic", "Normal"] },
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(
+    "<p> " + pokemonList[i].name + " " + pokemonList[i].height + "</p>"
+  );
+}
