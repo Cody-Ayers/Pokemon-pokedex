6238b05639d28a31e970037fb0168ea87de1bf3d
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2023-04-11 13:34:10 -0600

    Updated script.js with an IIFE as well as a forEach loop

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
 //This is the start of my script
 
-let pokemonList = [
-  { name: "Charmander", height: 0.6, type: ["Fire"] },
-  { name: "Squirtle", height: 0.5, type: ["Water"] },
-  { name: "Nidoqueen", height: 1.3, type: ["Ground", "Poison"] },
-  { name: "Venomoth", height: 1.5, type: ["Bug", "Poison"] },
-  { name: "Girafarig", height: 1.5, type: ["Psychic", "Normal"] },
-];
+// This is my IIFE
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Charmander", height: 0.6, type: ["Fire"] },
+    { name: "Squirtle", height: 0.5, type: ["Water"] },
+    { name: "Nidoqueen", height: 1.3, type: ["Ground", "Poison"] },
+    { name: "Venomoth", height: 1.5, type: ["Bug", "Poison"] },
+    { name: "Girafarig", height: 1.5, type: ["Psychic", "Normal"] },
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
 
 //for loop to print name and height
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.0) {
+pokemonRepository.getAll().forEach(function (pokemonList) {
+  if (pokemonList.height > 1.0) {
     document.write(
       "<p>" +
-        pokemonList[i].name +
-        " " +
-        pokemonList[i].height +
-        "- Wow that's huge!",
+        pokemonList.name +
+        " is " +
+        pokemonList.height +
+        "-Wow that's huge!",
       "</p>"
     );
   } else {
     document.write(
-      "<p>" + pokemonList[i].name + " " + pokemonList[i].height + "</p>"
+      "<p>" + pokemonList.name + " is " + pokemonList.height + "</p>"
     );
   }
-}
+});
